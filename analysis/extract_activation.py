8870f31eb2efa79e48bff3ef02c7ec105c34f290
mirkzx04 <bonifazi.mirko1@gmail.com>
2026-04-13 11:03:55 +0200

    Adding models iteration and representation extraction from each models

--- analysis/extract_activation.py
+++ analysis/extract_activation.py
@@ -15,37 +15,53 @@ device = 'cuda' if th.cuda.is_available() else 'cpu'
 rlvr_model = gsm8k_rlvr_model()
 sftt_model = gsm8k_sftt_model()
 base_model = vanilla_model()
+models = [(base_model, 'base'), (sftt_model, 'sftt'), (rlvr_model, 'rlvr')]
 
 # Setting system prompt
 SYSTEM_PROMPT = 'Solve this math problem thinking step by step'
+prompts = []
 
-def forward_with_cache(model, prompt):
+def forward_with_cache(model):
     with th.no_grad():
-        _, cache = model.run_with_cache(model, prompt) # Forward with caching to extract representation
+        _, cache = model.run_with_cache(prompts) # Forward with caching to extract representation
 
     return cache
 
-def extract_mlp_attn(model, prompt, last_token = True):
-    n_layers = model.cfg.n_layers
-
+def extract_mlp_attn(last_token = True):
     attn_outputs = {}
     mlp_outputs = {}
 
-    model_cache = forward_with_cache(model, prompt)
+    models_act = {}
 
-    # Extract attn and mlp representation for each layers
-    for l in range(n_layers):
-        if last_token : 
-            # Shape : [B, d_model]
-            attn_out = model_cache['attn_out', l][:, -1, :].detach().cpu()
-            mlp_out = model_cache['mlp_out', l][:, -1, :].detach().cpu()
-        else :
-            # Shape : [B, pos, d_model]
-            attn_out = model_cache['attn_out', l].detach().cpu()
-            mlp_out = model_cache['mlp_out', l].detach().cpu()
+    for couple in models:
+        model = couple[0]
+        model_name = couple[1]
+        n_layers = model.cfg.n_layers
 
-        attn_outputs[l] = attn_out
-        mlp_outputs[l] = mlp_out
 
-    return attn_outputs, mlp_outputs
+        model_cache = forward_with_cache(model)
+
+        # Extract attn and mlp representation for each layers
+        for l in range(n_layers):
+            if last_token : 
+                # Shape : [B, d_model]
+                attn_out = model_cache['attn_out', l][:, -1, :].detach().cpu()
+                mlp_out = model_cache['mlp_out', l][:, -1, :].detach().cpu()
+            else :
+                # Shape : [B, pos, d_model]
+                attn_out = model_cache['attn_out', l].detach().cpu()
+                mlp_out = model_cache['mlp_out', l].detach().cpu()
+
+            attn_outputs[l] = attn_out
+            mlp_outputs[l] = mlp_out
+
+        models_act[model_name] = {
+            'mlp_act' : mlp_outputs, 
+            'attn_act' : attn_outputs
+        }
+
+        attn_outputs = {}
+        mlp_outputs = {}
+
+    return models_act
 
