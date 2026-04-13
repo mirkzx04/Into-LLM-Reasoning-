b331fd6b37a1c22afc04f5f5d2a52cf83823a0c9
mirkzx04 <bonifazi.mirko1@gmail.com>
2026-04-13 23:49:06 +0200

    Fixing some bugs

--- models/model_wrapper.py
+++ models/model_wrapper.py
@@ -2,14 +2,14 @@ from peft import PeftModel
 
 from models.model import get_model
 
-def gsm8k_rlvr_model(model_pth = 'rlvr_GMS8K_model'):
+def gsm8k_rlvr_model(model_pth='rlvr_GMS8K_model'):
     model, lora_confg = get_model()
-    model_rlvr = model.from_pretrained(model, model_pth)
+    model_rlvr = PeftModel.from_pretrained(model, model_pth)
     return model_rlvr.merge_and_unload(), lora_confg
 
-def gsm8k_sftt_model(model_pth = 'sftt_GMS8K_model'):
+def gsm8k_sftt_model(model_pth='sftt_GMS8K_model'):
     model, lora_confg = get_model()
-    model_sftt = model.from_pretrained(model, model_pth)
+    model_sftt = PeftModel.from_pretrained(model, model_pth)
     return model_sftt.merge_and_unload(), lora_confg
 
 def vanilla_model():
