3bbb035d77ad7db681ec717687ee04d62b0d3ba3
mirkzx04 <bonifazi.mirko1@gmail.com>
2026-04-16 15:30:24 +0200

    Sperating get lora, get tokenizer and get model - adding experto merged model

--- models/model.py
+++ models/model.py
@@ -1,22 +1,39 @@
 import torch
 
 from peft import LoraConfig
-from transformers import AutoModelForCausalLM
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-def get_model():
-    # Define LoRA instructions 
-    peft_confing = LoraConfig(
+MODEL_ID = 'Qwen/Qwen2.5-3B'
+
+def get_lora():
+    return LoraConfig(
         r = 16, 
         lora_alpha=32,
-        target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"],
-        task_type="CAUSAL_LM",
+        target_modules=[
+            "q_proj",
+            "k_proj", 
+            "v_proj", 
+            "o_proj", 
+            "gate_proj", 
+            "up_proj", 
+            "down_proj"
+        ],
+        task_type='CAUSAL_LM'
     )
 
-    MODEL_ID = 'Qwen/Qwen2.5-3B'
-    model = AutoModelForCausalLM.from_pretrained(
+def get_tokenizer():
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
+
+    # Check padding token
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token 
+    
+    tokenizer.padding_side = 'left'
+    return tokenizer
+
+def get_model():
+    return AutoModelForCausalLM.from_pretrained(
         MODEL_ID, 
         torch_dtype = torch.bfloat16,
         attn_implementation = 'sdpa',
     )
-
-    return model, peft_confing
--- models/model_wrapper.py
+++ models/model_wrapper.py
@@ -2,15 +2,22 @@ from peft import PeftModel
 
 from models.model import get_model
 
-def gsm8k_rlvr_model(model_pth='rlvr_GMS8K_model'):
-    model, lora_confg = get_model()
+def gsm8k_rlvr_model(model_pth='adapters/rlvr_GMS8K_adapter'):
+    model = get_model()
     model_rlvr = PeftModel.from_pretrained(model, model_pth)
-    return model_rlvr.merge_and_unload(), lora_confg
+    return model_rlvr.merge_and_unload()
 
-def gsm8k_sftt_model(model_pth='sftt_GMS8K_model'):
-    model, lora_confg = get_model()
+def gsm8k_sftt_model(model_pth='adapters/sftt_GMS8K_adapter'):
+    model = get_model()
     model_sftt = PeftModel.from_pretrained(model, model_pth)
-    return model_sftt.merge_and_unload(), lora_confg
+    return model_sftt.merge_and_unload()
 
-def vanilla_model():
-    return get_model()
+def load_merged_model(adapter_dir):
+    model = get_model()
+    model = PeftModel.from_pretrained(model, adapter_dir)
+    return model.merge_and_unload()
+
+def export_merged_model(output_dir, adapter_dir):
+    model = load_merged_model(adapter_dir)
+    model.save_pretrained(output_dir)
+
