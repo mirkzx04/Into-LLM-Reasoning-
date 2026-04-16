ddfcf9389b595d792050efb642046787ab68ce29
mirkzx04 <bonifazi.mirko1@gmail.com>
2026-04-16 23:05:01 +0200

    Fixing some bugs and optimizing code

--- analysis/build_proibing_dataset.py
+++ analysis/build_proibing_dataset.py
@@ -4,22 +4,25 @@ parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(parent_dir)
 
 import pandas as pd
-import numpy as np
 import torch as th
 
 from math_verify import parse, verify
 from datasets import load_dataset
+from tqdm import tqdm
 
-from analysis.extract_activation import extract_residual_out
-
-from models.model import get_tokenizer
-from models.model_wrapper import gsm8k_rlvr_model, gsm8k_sftt_model, get_model
+from analysis.extract_activation import (
+    extract_residual_out, 
+    hf_base, 
+    hf_sftt,
+    hf_rlvr,
+    tokenizer
+)
 
 from GMS8K_logic.rlvr_pipeline.rewards_utils import extract_answer
 
 def generate_text(model, tokenizer, prompt, max_tokens = 512):
     device = next(model.parameters()).device
-    inputs = tokenizer(prompt, return_tensor = 'pt').to(device)
+    inputs = tokenizer(prompt, return_tensors = 'pt').to(device)
 
     with th.no_grad():
         out = model.generate(
@@ -30,25 +33,22 @@ def generate_text(model, tokenizer, prompt, max_tokens = 512):
             eos_token_id = tokenizer.eos_token_id
         )
     new_tokens = out[0, inputs['input_ids'].shape[1]:]
-    return tokenizer.decode(new_tokens, skip_spacial_tokens = True)
+    return tokenizer.decode(new_tokens, skip_special_tokens = True)
+
+device = 'cuda' if th.cuda.is_available() else 'cpu'
 
 platinum = load_dataset("madrylab/gsm8k-platinum", "main", split="test")
-
-tokenizer = get_tokenizer()
-rlvr_model = gsm8k_rlvr_model()
-sftt_model = gsm8k_sftt_model()
-base_model = get_model()
 
 rlvr_rows = []
 sftt_rows = []
 base_rows = []
 
-for q, a in zip(platinum['question'], platinum['answer']):
+for q, a in tqdm(zip(platinum['question'], platinum['answer'])):
     gt = extract_answer(a)
 
-    rlvr_answer = extract_answer(rlvr_model.generate(q))
-    sftt_answer = extract_answer(sftt_model.generate(q))
-    base_answer = extract_answer(base_model.generate(q))
+    rlvr_answer = extract_answer(generate_text(hf_rlvr, tokenizer, q))
+    sftt_answer = extract_answer(generate_text(hf_sftt, tokenizer, q))
+    base_answer = extract_answer(generate_text(hf_base, tokenizer, q))
 
     correct_rlvr = (
         rlvr_answer is not None and gt is not None
@@ -70,7 +70,7 @@ for q, a in zip(platinum['question'], platinum['answer']):
         'ground_truth' : gt,
         'model_answer' : rlvr_answer,
         'correct' : correct_rlvr,
-        'resid_post' : models_residuals['rlvr']['resid_post_outs'],
+        'resid_pre' : models_residuals['rlvr']['resid_pre_outs'],
         'resid_attn_mlp' : models_residuals['rlvr']['resid_attn_mlp_outs'],
         'resid_attn' : models_residuals['rlvr']['resid_attn_outs']
     })
@@ -79,7 +79,7 @@ for q, a in zip(platinum['question'], platinum['answer']):
         'ground_truth' : gt,
         'model_answer' : sftt_answer,
         'correct' : correct_sftt,
-        'residual_post' : models_residuals['sftt']['resid_post_outs'],
+        'resid_pre' : models_residuals['sftt']['resid_pre_outs'],
         'resid_attn_mlp' : models_residuals['sftt']['resid_attn_mlp_outs'],
         'resid_attn' : models_residuals['sftt']['resid_attn_outs']
     })
@@ -88,7 +88,7 @@ for q, a in zip(platinum['question'], platinum['answer']):
         'ground_truth' : gt,
         'model_answer' : base_answer,
         'correct' : correct_base,
-        'residual_post' : models_residuals['base']['resid_post_outs'],
+        'resid_pre' : models_residuals['base']['resid_pre_outs'],
         'resid_attn_mlp' : models_residuals['base']['resid_attn_mlp_outs'],
         'resid_attn' : models_residuals['base']['resid_attn_outs']
     })
@@ -96,7 +96,3 @@ for q, a in zip(platinum['question'], platinum['answer']):
 pd.DataFrame(rlvr_rows).to_pickle('df_rlvr.pkl')
 pd.DataFrame(sftt_rows).to_pickle('df_sftt.pkl')
 pd.DataFrame(base_rows).to_pickle('df_base.pkl')
-    
-
-    
-
--- analysis/extract_activation.py
+++ analysis/extract_activation.py
@@ -21,24 +21,30 @@ ADAPTER_DIR = 'adapters'
 RLVR_ADAPTER_DIR = f'{ADAPTER_DIR}/rlvr_GMS8K_adapter'
 SFTT_ADAPTER_DIR = f'{ADAPTER_DIR}/sftt_GMS8K_adapter'
 
+hf_base = get_model().to(device).eval()
+hf_sftt = load_merged_model(SFTT_ADAPTER_DIR).to(device).eval()
+hf_rlvr = load_merged_model(RLVR_ADAPTER_DIR).to(device).eval()
+
+tokenizer = get_tokenizer()
+
 base_tl = HookedTransformer.from_pretrained_no_processing(
     MODEL_ID,
-    hf_model = get_model(),
-    tokenizer = get_tokenizer(),
+    hf_model = hf_base,
+    tokenizer = tokenizer,
     device = device,
     dtype=th.bfloat16,
 )
 sff_tl = HookedTransformer.from_pretrained_no_processing(
     MODEL_ID,
-    hf_model = load_merged_model(SFTT_ADAPTER_DIR),
-    tokenizer = get_tokenizer(),
+    hf_model = hf_sftt,
+    tokenizer = tokenizer,
     device = device,
     dtype=th.bfloat16,
 )
 rlvr_tl = HookedTransformer.from_pretrained_no_processing(
     MODEL_ID,
-    hf_model = load_merged_model(RLVR_ADAPTER_DIR),
-    tokenizer = get_tokenizer(),
+    hf_model = hf_rlvr,
+    tokenizer = tokenizer,
     device = device,
     dtype=th.bfloat16,
 )
@@ -52,9 +58,13 @@ models = [
 # Setting system prompt
 SYSTEM_PROMPT = 'Solve this math problem thinking step by step'
 
-def forward_with_cache(model, prompts):
+def forward_with_cache(model, prompts, last_token, names_filter = None):
     with th.no_grad():
-        _, cache = model.run_with_cache(prompts) # Forward with caching to extract representation
+        _, cache = model.run_with_cache(
+            prompts,
+            names_filter = names_filter,
+            pos_slice = -1 if last_token else None
+        ) # Forward with caching to extract representation
 
     return cache
 
@@ -69,20 +79,14 @@ def extract_mlp_attn_out(prompts, last_token = True):
         model_name = couple[1]
         n_layers = model.cfg.n_layers
 
-
-        model_cache = forward_with_cache(prompts, model)
+        model_cache = forward_with_cache(model, prompts, last_token)
 
         # Extract attn and mlp representation for each layers
         for l in range(n_layers):
-            if last_token : 
-                # Shape : [B, d_model]
-                attn_out = model_cache['attn_out', l][:, -1, :].detach().cpu()
-                mlp_out = model_cache['mlp_out', l][:, -1, :].detach().cpu()
-            else :
-                # Shape : [B, pos, d_model]
-                attn_out = model_cache['attn_out', l].detach().cpu()
-                mlp_out = model_cache['mlp_out', l].detach().cpu()
-
+            # Shape : [B, d_model] or [B, pos, d_mod
+            attn_out = model_cache['attn_out', l].detach().cpu()
+            mlp_out = model_cache['mlp_out', l].detach().cpu()
+        
             attn_outputs[l] = attn_out
             mlp_outputs[l] = mlp_out
 
@@ -97,43 +101,38 @@ def extract_mlp_attn_out(prompts, last_token = True):
     return models_act
 
 def extract_residual_out(prompts, last_token = True):
-    
     model_residuals = {}
 
-    for couple in models:
-        resid_post_outs = {}
+    for model, model_name in models:
+        n_layers = model.cfg.n_layers
+        interesting_layers = [0, n_layers // 2, n_layers - 1]
+
+        hook_names =[]
+        for l in interesting_layers:
+            hook_names.append(f'blocks.{l}.hook_resid_pre')
+            hook_names.append(f'blocks.{l}.hook_resid_mid')
+            hook_names.append(f'blocks.{l}.hook_resid_post')
+
+        model_cache = forward_with_cache(model, prompts, last_token, hook_names)
+        
+        resid_pre_outs = {}
         resid_attn_outs = {}
         resid_attn_mlp_outs = {}
 
-        model = couple[0]
-        model_name = couple[1]
+        for hook in hook_names:
+            # Shape : [B, d_model] or [B, pos, d_model]
+            resid_pre = model_cache[hook].detach().cpu()
+            resid_attn = model_cache[hook].detach().cpu()
+            resid_attn_mlp = model_cache[hook].detach().cpu()
 
-        model_cache = forward_with_cache(prompts, model)
-        n_layers = model.cfg.n_layers
-        
-        interesting_layers = [0, n_layers // 2, n_layers - 1]
-
-        for l in interesting_layers:
-            if last_token:
-                # Shape : [B, d_model]
-                resid_post = model_cache['resid_post', l][:, -1, :].detach().cpu()
-                resid_attn = model_cache['resid_mid', l][:, -1, :].detach().cpu()
-                resid_attn_mlp = model_cache['resid_post', l][:, -1, :].detach().cpu()
-
-            else :
-                # Shape : [B, post, d_model]
-                resid_post = model_cache['resid_post', l].detach().cpu()
-                resid_attn = model_cache['resid_mid', l].detach().cpu()
-                resid_attn_mlp = model_cache['resid_post', l].detach().cpu()
-
-            resid_post_outs[l] = resid_post 
+            resid_pre_outs[l] = resid_pre 
             resid_attn_outs[l] = resid_attn
             resid_attn_mlp_outs[l] = resid_attn_mlp
                         
-            model_residuals[model_name] = {
-                'resid_post_outs' : resid_post_outs,
-                'resid_attn_outs' : resid_attn_outs, 
-                'resid_attn_mlp_outs' : resid_attn_mlp_outs
-            }
+        model_residuals[model_name] = {
+            'resid_pre_outs' : resid_pre_outs,
+            'resid_attn_outs' : resid_attn_outs, 
+            'resid_attn_mlp_outs' : resid_attn_mlp_outs
+        }
 
     return model_residuals
