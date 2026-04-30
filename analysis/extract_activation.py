47e58fa7fb230a2a891b67e0862ff07de11c457a
mirkzx04 <bonifazi.mirko1@gmail.com>
2026-04-30 11:27:09 +0200

    Adding prompt, completion and pre-completion extrac activation

--- analysis/extract_activation.py
+++ analysis/extract_activation.py
@@ -9,7 +9,7 @@ import models.rope_theta
 
 from transformers import AutoConfig
 from transformer_lens import HookedTransformer
-from pathlib import Path
+from tqdm import tqdm
 
 from models.model import MODEL_ID, get_model, get_tokenizer
 
@@ -67,12 +67,58 @@ models = [
     (rlvr_tl, 'rlvr')
 ]
 
-def forward_with_cache(model, prompts, last_token, names_filter = None):
+def generate_rlvr_reasoning(prompts, max_new_tokens = 2000, do_sample = False, temperature = 0.7, top_p = 0.95, batch_size = 5):
+    prompts_ids = tokenizer(
+        prompts,
+        return_tensor = "pt",
+        add_special_token = False,
+    ).inputs_ids.to(device)
+
+    with th.inference_mode():
+        full_ids = rlvr_tl.generate(
+            prompts_ids,
+            max_new_tokens=max_new_tokens,
+            stop_at_eos=True,
+            do_sample=do_sample,
+            temperature=temperature,
+            top_p=top_p,
+            return_type="tokens",
+            verbose=False
+        )
+
+    completion_ids = full_ids[:, prompts_ids.shape[-1]:]
+    return prompts_ids, completion_ids, full_ids
+
+def get_output_token_position(prompt_len, completion_len, mode):
+    """
+    mode = "state" 
+        Take the positions of the completion tokens already present in the sequence
+
+    mode = "predictive"
+        takes positions that predict completion tokens
+
+    mode = "prompt" 
+        rakes the positions of input tokens
+    """
+    if mode == "state":
+        start = prompt_len
+        end = prompt_len + completion_len
+
+    elif mode == "predictive":
+        start = prompt_len - 1
+        end = prompt_len + completion_len - 1
+    elif mode == "prompt": 
+        start = 0
+        end = prompt_len
+
+    return list(range(start, end))
+
+def forward_with_cache(model, prompts, positions, names_filter = None):
     with th.no_grad():
         _, cache = model.run_with_cache(
             prompts,
             names_filter = names_filter,
-            pos_slice = -1 if last_token else None
+            pos_slice = positions
         ) # Forward with caching to extract representation
 
     return cache
@@ -84,23 +130,53 @@ def get_hooks(interesting_layers, blocks):
         for b in blocks
     ]
 
-def extract_mlp_attn_out(prompts, interesting_layers, blocks, last_token = True):
-    models_act = {}
-
+def extract_mlp_attn_out(prompts, interesting_layers, blocks, max_new_tokens, generation_do_sample = False, cache_mode = "state"):
     hook_names = get_hooks(interesting_layers, blocks)
 
-    for model, model_name in models:
-        model_cache = forward_with_cache(model, prompts, last_token, hook_names)
+    prompt_ids, completion_ids, full_ids = generate_rlvr_reasoning(
+        prompts=prompts,
+        max_new_tokens=max_new_tokens,
+        do_sample=generation_do_sample
+    ) # Generate
+
+    prompt_len = prompt_ids.shape[-1]
+    completion_len = completion_ids.shape[-1]
+
+    positions = get_output_token_position(
+        prompt_len=prompt_len,
+        completion_len=completion_len,
+        mode = cache_mode
+    )
+
+    out = {
+        "prompt": prompts,
+        "prompt_ids": prompt_ids.detach().cpu(),
+        "completion_ids": completion_ids.detach().cpu(),
+        "full_ids": full_ids.detach().cpu(),
+        "positions": positions,
+        "cache_mode": cache_mode,
+        "activations": {},
+    }
+
+    for model, model_name in models: 
+        model_cache = forward_with_cache(
+            model = model,
+            input_ids = full_ids,
+            positions=positions,
+            names_filter=hook_names
+        )
+
         layer_act = {}
-
-        # Extract activation
         for l in interesting_layers:
-            # Shape : [B, d_model] or [B, pos, d_mod]
             layer_act[l] = {
-                block : model_cache[f"blocks.{l}.{block}"] 
+                block : model_cache[f"blocks.{l}.{block}"].detach().cpu()
                 for block in blocks
-            } 
+            }
+        out["activations"][model_name] = layer_act
+    
+        del model_cache
+        th.cuda.empty_cache()
 
-        models_act[model_name] = layer_act
+    return out
 
-    return models_act
+    
